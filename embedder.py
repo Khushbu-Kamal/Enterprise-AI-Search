6bd2906340e78ff4daeca243e6e7520b1d79d72a
Khushbu Kamal <khushbu.kamal02@outlook.com>
2026-05-21 23:39:30 +0530

    Add files via upload

---
+++ embedder.py
@@ -0,0 +1,16 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer(
+    "all-MiniLM-L6-v2"
+)
+
+def generate_embeddings(chunks):
+
+    texts = [
+        chunk.page_content
+        for chunk in chunks
+    ]
+
+    vectors = model.encode(texts)
+
+    return vectors
